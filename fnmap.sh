cfe9f64b53309162086b624b01fbb4020acfdf82
vflame6 <multi.zadiak@gmail.com>
2022-12-23 16:10:00 +0300

    Added fnmap.sh, web.sh, listener.sh v2

---
+++ fnmap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This is a bash script to scan all TCP/UDP ports on the machine and give nice browser-based output
+# It runs nmap 2 times:
+# First to get all ports on the machine
+# Second for deeper scan of the ports found
+
+if [[ $# != 1 ]]
+then
+    echo -e "\e[0;31m[!]\e[0m Not specified a host or incorrect use."
+    exit 1
+fi
+
+echo -n "Treat all hosts as online -- skip host discovery (Y/N)? "
+read answer
+PN=""
+if [ "$answer" != "${answer#[Yy]}" ] ; then
+    PN="-Pn";
+fi
+
+ports=$(sudo nmap -p- -sU -sT $PN --min-rate=500 $1 | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+echo "Ports found:
+$ports
+"
+filename="$1_scan" 
+sudo nmap -p$ports -sU -sT -T4 $PN -sC -sV -oA $filename --stylesheet https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl $1
---
+++ web.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# This is a bash script for quickly setting up a web server.
+# It generates a random not busy port and setting up a Python web server
+# Also, it enumerates all network interfaces to format the http links to that web server with the port
+
+while
+  port=$(shuf -n 1 -i 4000-10000)
+  netstat -atun | grep -q "$port"
+do
+  continue
+done
+
+echo -e "\e[0;36m[*]\e[0m Starting web server on port $port"
+echo -e "\e[0;36m[*]\e[0m Links:"
+
+
+while read -r interface address
+do
+  addr=$(echo $address | cut -d '/' -f 1)  
+  echo "  $interface: http://$addr:$port/"
+done < <(ip -o a show | cut -d ' ' -f 2,7 | grep -v '::')
+
+python3 -m http.server $port > /dev/null
