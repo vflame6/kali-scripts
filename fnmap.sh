bc0ecfefa133289330203a37b3799b2db6b66d9d
vflame6 <multi.zadiak@gmail.com>
2022-12-28 08:56:03 -0500

    Added a question to perform UDP ports scanning or not

--- fnmap.sh
+++ fnmap.sh
@@ -16,15 +16,24 @@ then
 fi
 
 echo -n "Treat all hosts as online -- skip host discovery (Y/N)? "
-read answer
+read pn_answer
 PN=""
-if [ "$answer" != "${answer#[Yy]}" ] ; then
+
+if [ "$pn_answer" != "${pn_answer#[Yy]}" ] ; then
     PN="-Pn";
 fi
 
-ports=$(sudo nmap -p- -sU -sT $PN --min-rate=500 $1 | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+echo -n "Scan UDP ports (Y/N)?
+read udp_answer
+UDP=""
+
+if [ "$udp_answer" != "${udp_answer#[Yy]}" ; then
+    UDP="-sU"
+fi
+
+ports=$(sudo nmap -p- $UDP -sT $PN --min-rate=500 $1 | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
 echo "Ports found:
 $ports
 "
 filename="$1_scan" 
-sudo nmap -p$ports -sU -sT -T4 $PN --open -sC -sV -oA $filename --stylesheet https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl $1
+sudo nmap -p$ports $UDP -sT -T4 $PN --open -sC -sV -oA $filename --stylesheet https://raw.githubusercontent.com/honze-net/nmap-bootstrap-xsl/master/nmap-bootstrap.xsl $1
