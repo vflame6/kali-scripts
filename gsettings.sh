fc23b19580799129718a2b1dc721e4a981eba491
vflame6 <80809363+vflame6@users.noreply.github.com>
2024-06-12 22:12:32 +0300

    Removed not working features from install script

---
+++ gsettings.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NOCOLOR="\e[0m"
+LIGHT_BLUE="\e[36m"
+
+echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Configuring Ubuntu native settings"
+
+dconf write /org/gnome/desktop/sound/event-sounds "false"
+gsettings set org.gnome.desktop.notifications show-banners false
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.wm.preferences audible-bell false
+gsettings set org.gnome.desktop.wm.preferences visual-bell false
+gsettings set org.gnome.desktop.interface toolbar-icons-size 'small'
+gsettings set org.gnome.shell.extensions.ding icon-size 'small'
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
--- install_offensiveubuntu.sh
+++ install_offensiveubuntu.sh
@@ -6,6 +6,7 @@
 #
 # usage: 
 # curl https://raw.githubusercontent.com/vflame6/kali-scripts/main/install_offensiveubuntu.sh|sudo bash
+#
 
 NOCOLOR="\e[0m"
 LIGHT_BLUE="\e[36m"
@@ -15,23 +16,25 @@ cd /opt/
 # PACKAGES
 
 echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Updating packages"
+
+# resolve graphics problem
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+
 sudo apt update
 sudo apt -y full-upgrade
-sudo apt -y autoremove
 
 echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Installing packages"
-sudo apt-get install -y open-vm-tools open-vm-tools-desktop
-sudo apt install -y vim wget curl git unzip pv
-sudo apt install -y apache2 postgresql tmux openvpn samba openssh-client openssh-server
-sudo apt install -y smbclient rdesktop freerdp2-x11
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev python3 python3-pip
-sudo apt install -y copyq
-sudo apt install -y pipx && pipx ensurepath && sudo pipx ensurepath --global
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo apt-get install -y open-vm-tools open-vm-tools-desktop
+sudo apt install -y vim wget curl git unzip
+sudo apt install -y apache2 postgresql tmux openvpn samba
+sudo apt install -y smbclient rdesktop freerdp2-x11
+sudo apt install -y python3 python3-pip
+sudo apt install -y copyq
+
+sudo apt install -y pipx
+sudo pipx ensurepath
+sudo -u $SUDO_USER pipx ensurepath
 
 sudo apt-get install -y openjdk-11-jdk apt-transport-https
 sudo apt-get install wget curl nano software-properties-common dirmngr apt-transport-https gnupg gnupg2 ca-certificates lsb-release ubuntu-keyring unzip -y
@@ -49,6 +52,7 @@ wget https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.3/jython-st
 # DOCKER
 
 echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Installing docker"
+
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -65,36 +69,34 @@ sudo usermod -aG docker $SUDO_USER
 # DOCKER CONTAINERS
 
 echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Pulling containers"
+
 sudo docker pull python:2.7.18-stretch
 
 # TOOLS
 
 echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Installing tools"
-sudo pipx install git+https://github.com/Pennyw0rth/NetExec
-sudo pipx install impacket
+
+sudo -u $SUDO_USER pipx install git+https://github.com/Pennyw0rth/NetExec
+sudo -u $SUDO_USER pipx install impacket
+
+sudo -u $SUDO_USER pip3 install impacket
 
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go get github.com/ropnop/kerbrute
 
-wget https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.1/BloodHound-linux-x64.zip \ 
-  && unzip BloodHound-linux-x64.zip | pv -l >/dev/null \
-  && rm -f BloodHound-linux-x64.zip
+wget https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.1/BloodHound-linux-x64.zip
+unzip BloodHound-linux-x64.zip
+rm -f BloodHound-linux-x64.zip
 
 mkdir -p /opt/wordlists
 curl -fsSL https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt -o /opt/wordlists/rockyou.txt
 cd /opt/wordlists/
-wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip \
-  && unzip SecList.zip | pv -l >/dev/null \
-  && rm -f SecList.zip
-cd /opt/
 
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   ./msfinstall && \
   rm -f msfinstall
-
-curl https://sliver.sh/install|sudo bash
 
 # CONF FILES
 
@@ -112,16 +114,12 @@ chmod +x /opt/aliases/fnmap.sh
 sudo curl -fsSL https://raw.githubusercontent.com/vflame6/kali-scripts/main/web.sh -o /opt/aliases/web.sh
 chmod +x /opt/aliases/web.sh
 
-echo "vbell off" >> /home/$SUDO_USER/.screenrc
-echo "bell_msg \"\"" >> /home/$SUDO_USER/.screenrc
-
 echo >> /etc/bash.bashrc
 echo "bind 'set bell-style none'" >> /etc/bash.bashrc
 echo "alias grep='grep --color=auto'" >> /etc/bash.bashrc
 echo "alias ll='ls -lsah'" >> /etc/bash.bashrc
-echo "alias xclip='xclip -selection clipboard'" >> /etc/bash.bashrc
 echo "alias rev='/opt/aliases/rev.sh'" >> /etc/bash.bashrc
-echo "alias web=/opt/aliases/web.sh'" >> /etc/bash.bashrc
+echo "alias web='/opt/aliases/web.sh'" >> /etc/bash.bashrc
 echo "alias updatesys='/opt/aliases/updatesys.sh'" >> /etc/bash.bashrc
 echo "alias fnmap='/opt/aliases/fnmap.sh'" >> /etc/bash.bashrc
 
@@ -138,27 +136,6 @@ systemctl stop cups && systemctl disable cups
 systemctl stop apache2 && systemctl disable apache2
 systemctl stop sliver && systemctl disable sliver
 systemctl stop postgresql && systemctl disable postgresql
-systemctl stop ssh && systemctl disable ssh
-
-# UBUNTU CONFIGURATION
-
-echo -e "${LIGHT_BLUE}[*]${NOCOLOR} Configuring Ubuntu native settings"
-
-dconf write /org/gnome/desktop/sound/event-sounds "false"
-sudo -u gdm gsettings set org.gnome.desktop.notifications show-banners false
-sudo -u gdm gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-sudo -u gdm gsettings set org.gnome.desktop.wm.preferences audible-bell false
-sudo -u gdm gsettings set org.gnome.desktop.wm.preferences visual-bell false
-sudo -u gdm gsettings set org.gnome.desktop.interface toolbar-icons-size 'small'
-sudo -u gdm gsettings set org.gnome.shell.extensions.ding icon-size 'small'
-sudo -u gdm gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
-sudo -u gdm gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
-sudo -u gdm gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-sudo -u gdm gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-
-if [ -f /usr/share/backgrounds/canvas_by_roytanck.jpg ]; then
-  sudo -u gdm gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/canvas_by_roytanck.jpg
-fi
 
 sudo -u $SUDO_USER copyq --start-server config autostart true
 sudo -u $SUDO_USER copyq config tray_items 10
